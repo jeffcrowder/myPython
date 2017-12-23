5dd0b545b17d2af9614caa01165826b103913ea5
Jeff Crowder <jeffcrowder1234@gmail.com>
2017-12-23 00:27:38 -0600

    Got the cheesy game working

--- exampleCode/gameboard.py
+++ exampleCode/gameboard.py
@@ -24,37 +24,48 @@ class GameBoard:
             [c.CBLACK,c.CBLACK,c.CBLACK,c.CBLACK ],[c.CBLACK,c.CBLACK,c.CBLACK,c.CBLACK ], \
             [c.CBLACK,c.CBLACK,c.CBLACK,c.CBLACK ],[c.CBLACK,c.CBLACK,c.CBLACK,c.CBLACK ]]
     
+    clueColorList = [\
+            [c.CBLACK,c.CBLACK,c.CBLACK,c.CBLACK ],[c.CBLACK,c.CBLACK,c.CBLACK,c.CBLACK ], \
+            [c.CBLACK,c.CBLACK,c.CBLACK,c.CBLACK ],[c.CBLACK,c.CBLACK,c.CBLACK,c.CBLACK ], \
+            [c.CBLACK,c.CBLACK,c.CBLACK,c.CBLACK ],[c.CBLACK,c.CBLACK,c.CBLACK,c.CBLACK ], \
+            [c.CBLACK,c.CBLACK,c.CBLACK,c.CBLACK ],[c.CBLACK,c.CBLACK,c.CBLACK,c.CBLACK ], \
+            [c.CBLACK,c.CBLACK,c.CBLACK,c.CBLACK ],[c.CBLACK,c.CBLACK,c.CBLACK,c.CBLACK ]]
+
     thisGuess = [c.CBLACK,c.CBLACK,c.CBLACK,c.CBLACK]
+    thisClue = [c.CBLACK,c.CBLACK,c.CBLACK,c.CBLACK]
+
+    winFlag = False
+    
+    answer = [c.CRED,c.CWHITE,c.CCYAN,c.CYELLOW]
     CBLACKBG = c.CBLACKBG
     ENDC = c.ENDC
 
     numRows = 10
     guessCount = 10
 
-    def latest_guess(self, colors):
+    def update_guess(self, colors):
         self.thisGuess = colors
         self.guessCount -= 1
-        self.update_colorList(colors,self.guessCount)
-        #print(self.guessCount)
-    
-    def update_colorList(self,colors,cnt):
         for i in range(4):
-            self.colorList[cnt][i] = colors[i]
-            #print(str(self.colorList[cnt][i]))
-
+            self.colorList[self.guessCount][i] = colors[i]
+    
+    def update_clueColorList(self,numColors,numLocations):
+        # add the white pegs
+        for i in range(numColors):
+            self.clueColorList[self.guessCount][i] = self.c.CWHITE
+        # now change to green
+        for i in range(numLocations):
+            self.clueColorList[self.guessCount][i] = self.c.CGREEN
+        
     def user_guess(self):
+        print ""
         print "---------------------------------------------------------------------"
-        print ""
-        print ""
         print "Enter your guess for the four colors and position of the secret code."
         print "Please choose four of the six following colors: " 
-        print self.c.CWHITE  + "   w =white" 
-        print self.c.CRED    + "   r =red " 
-        print self.c.CGREEN  + "   g =green " 
-        print self.c.CYELLOW + "   y =yellow "
-        print self.c.CBLUE   + "   b =blue " 
-        print self.c.CCYAN   + "   c =cyan " + self.c.ENDC
-        print "---------------------------------------------------------------------"
+        print self.c.CWHITE  + "   w =white" + self.c.CRED + "   r =red"  
+        print self.c.CGREEN  + "   g =green" + self.c.CYELLOW + "   y =yellow" 
+        print self.c.CBLUE   + "   b =blue " + self.c.CCYAN + "   c =cyan" + self.c.ENDC 
+        print "---------------------------------------------------------------------\n"
         
         guess = [self.c.CBLACK,self.c.CBLACK,self.c.CBLACK,self.c.CBLACK] 
         x = 0
@@ -91,11 +102,33 @@ class GameBoard:
                 x += 1
             else:
                 print("you didnt pick a valid color!")
+        #self.check_guess(guess)
         return guess
+    
+    def check_guess(self,guess):
+        a = self.answer
+        g = guess
+        # this lets me know how many white pegs
+        color_count = len(list(set(a).intersection(g)))
+        # This lets me know ho many green pegs 
+        location_count = 0
+        for i,j in zip(a,g):
+            if i==j:
+                location_count += 1
+                print("location count")
+                print(location_count)
+        # set the win flag 
+        if location_count == 4:
+            self.winFlag = True
 
+        self.update_clueColorList(color_count, location_count)
+        print ""
+        
     def print_board(self):
+        print "---MASTERMIND---"
+        print "| X X X X |    |"
+        print "|---------|----|"
         for i in range(self.numRows):
-            #c = PrintColor()
             #build the guess string 
             gstr0 = ''.join([self.colorList[i][0], self.guessList[i][0]])
             gstr1 = ''.join([self.colorList[i][1], self.guessList[i][1]])
@@ -103,35 +136,36 @@ class GameBoard:
             gstr3 = ''.join([self.colorList[i][3], self.guessList[i][3], pc.ENDC])
             
             #build the clue string
-            #currently this is hard coded it needs to come from server
-            clueColor0 = self.c.CWHITE
-            clueColor1 = self.c.CGREEN
-            clueColor2 = self.c.CBLACK
-            clueColor3 = self.c.CBLACK
-            
-            cstr0 = ''.join([clueColor0, self.clueList[i][0]])
-            cstr1 = ''.join([clueColor1, self.clueList[i][1]])
-            cstr2 = ''.join([clueColor2, self.clueList[i][2]])
-            cstr3 = ''.join([clueColor3, self.clueList[i][3], pc.ENDC])
+            cstr0 = ''.join([self.clueColorList[i][0], self.clueList[i][0]])
+            cstr1 = ''.join([self.clueColorList[i][1], self.clueList[i][1]])
+            cstr2 = ''.join([self.clueColorList[i][2], self.clueList[i][2]])
+            cstr3 = ''.join([self.clueColorList[i][3], self.clueList[i][3], pc.ENDC])
             
             guessString = self.c.CBLACKBG + "| %s %s %s %s |" % (gstr0,gstr1,gstr2,gstr3) \
                 + self.c.CBLACKBG + "%s%s%s%s|" % (cstr0,cstr1,cstr2,cstr3) 
             print( guessString)
+        print "----------------\n"
 
 gb = GameBoard()
 
-pc = gb.c #PrintColor()
+pc = gb.c 
 
 print( chr(27) + "[2J" )
-print" test "
 
-gameCounter = 0
-while gameCounter < 10:
-    guess = gb.user_guess()
-    gb.latest_guess(guess)
-    gb.print_board()
-    gameCounter += 1
+try:
+    gameCounter = 0
+    while not gb.winFlag: 
+        if gameCounter == 10:
+            print(" You lost. Used all %s moves." % gameCounter)
+            break
+        guess = gb.user_guess()
+        gb.update_guess(guess)
+        gb.check_guess(guess)
+        gb.print_board()
+        gameCounter += 1
 
+    print(" You won in %s moves." % gameCounter)
 
-
+except KeyboardInterrupt:
+    print "Ending the Game"
 
